6ffd408d1a2d185754bab098bcda5d6620f05c66
ivonnatalia1107 <ivon.natalia.1107@gmail.com>
2023-12-19 12:10:44 -0400

    footer

--- app/layout.js
+++ app/layout.js
@@ -21,8 +21,8 @@ export default function RootLayout({ children }) {
        <Provider> 
         <HeaderPage/>
         {children} 
-        <Footer/>
         </Provider> 
+        <Footer/>
         </body>
     </html>
   )
